9f215a5051295a665b6b72647ccd2c1c2e2983eb
zhoumy <as20102010>
2017-09-06 23:25:18 +0800

    显示旺旺网页

--- app/src/main/java/cn/zmy/wangwangmessenger/MainActivity.java
+++ app/src/main/java/cn/zmy/wangwangmessenger/MainActivity.java
@@ -2,14 +2,55 @@ package cn.zmy.wangwangmessenger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity
 {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setTitle("登录淘宝");
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(false);
+        mWebView.getSettings().setAppCacheEnabled(false);
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mWebView.loadUrl("https://h5.m.taobao.com/ww/index.htm");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        CookieManager.getInstance().removeAllCookies(new ValueCallback<Boolean>()
+        {
+            @Override
+            public void onReceiveValue(Boolean value)
+            {
+
+            }
+        });
+        mWebView.clearCache(true);
+        mWebView.clearHistory();
+        mWebView.destroy();
     }
 }
---
+++ app/src/main/java/cn/zmy/wangwangmessenger/TheApplication.java
@@ -0,0 +1,16 @@
+package cn.zmy.wangwangmessenger;
+
+import android.app.Application;
+
+/**
+ * Created by zmy on 2017/9/6.
+ */
+
+public class TheApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
