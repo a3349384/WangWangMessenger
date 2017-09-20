3008ad9ed93f1dda632f26da65d7807f3dc523c7
zhoumy <as20102010>
2017-09-20 14:45:22 +0800

    完善：登录状态判断

--- app/src/main/java/cn/zmy/wangwangmessenger/MainActivity.java
+++ app/src/main/java/cn/zmy/wangwangmessenger/MainActivity.java
@@ -1,12 +1,12 @@
 package cn.zmy.wangwangmessenger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,6 +14,7 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity
 {
     private WebView mWebView;
+    private ScanCookieHandler mScanCookieHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setTitle("登录淘宝");
         setContentView(R.layout.activity_main);
+
+        //移除所有Cookie
+        CookieManager.getInstance().removeAllCookie();
 
         mWebView = (WebView) findViewById(R.id.webView);
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -35,22 +39,52 @@ public class MainActivity extends AppCompatActivity
             }
         });
         mWebView.loadUrl("https://h5.m.taobao.com/ww/index.htm");
+
+        mScanCookieHandler = new ScanCookieHandler();
+        mScanCookieHandler.sendEmptyMessageDelayed(1, 1000);
     }
 
     @Override
     protected void onDestroy()
     {
         super.onDestroy();
-        CookieManager.getInstance().removeAllCookies(new ValueCallback<Boolean>()
-        {
-            @Override
-            public void onReceiveValue(Boolean value)
-            {
-
-            }
-        });
+        //移除所有Cookie
+        CookieManager.getInstance().removeAllCookie();
         mWebView.clearCache(true);
         mWebView.clearHistory();
         mWebView.destroy();
+
+        if (mScanCookieHandler != null)
+        {
+            mScanCookieHandler.removeCallbacksAndMessages(null);
+            mScanCookieHandler = null;
+        }
+    }
+
+    private void onLoginSuccess()
+    {
+        startActivity(new Intent(this, MessageActivity.class));
+        finish();
+    }
+
+    private class ScanCookieHandler extends Handler
+    {
+        @Override
+        public void dispatchMessage(Message msg)
+        {
+            super.dispatchMessage(msg);
+            CookieManager cookieManager = CookieManager.getInstance();
+            String cookieString = cookieManager.getCookie("api.m.taobao.com");
+            if (cookieString != null && cookieString.contains("_m_h5_tk"))
+            {
+                //登录已成功
+                this.removeCallbacksAndMessages(null);
+                onLoginSuccess();
+            }
+            else
+            {
+                this.sendEmptyMessageDelayed(1, 1000);
+            }
+        }
     }
 }
---
+++ app/src/main/java/cn/zmy/wangwangmessenger/MessageActivity.java
@@ -0,0 +1,19 @@
+package cn.zmy.wangwangmessenger;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zmy on 2017/9/20.
+ */
+
+public class MessageActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setTitle("消息");
+    }
+}
